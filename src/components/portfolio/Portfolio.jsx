08798c07af4802020ff560a17a633ea476e7834d
Prince <Prince.chowdhury.246@my.csun.edu>
2023-05-03 01:59:56 -0700

    New projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,28 +1,28 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import React from "react";
 import "./portfolio.css";
-import IMG1 from "../../assets/10cd38d1d9f6f82dbc86b7addcaec7e2.webp";
-import IMG2 from "../../assets/5df5a84f3ed908ad7bad36fa645e3d4a.webp";
-import IMG3 from "../../assets/ac332cca20a284b8654e77da6c7102d2.webp";
+import IMG1 from "../../assets/Slack.png";
+import IMG2 from "../../assets/Hoobank.png";
+import IMG3 from "../../assets/linkedin.png";
 
 const data = [
   {
     id: 1,
     image: IMG3,
-    title: "Recipe Finder",
-    github: "https://github.com/jvsooon/recipe-finder",
+    title: "LinkedIn Clone",
+    github: "https://linkedin-clone-yt-6a785.web.app/",
   },
   {
     id: 2,
     image: IMG2,
-    title: "Blog Site",
-    github: "https://github.com/Space-Prince0821/Comp440",
+    title: "Hoobank",
+    github: "https://hoobank-test-site.netlify.app/",
   },
   {
     id: 3,
     image: IMG1,
-    title: "Giga Pet Game",
-    github: "https://comp484-pro-2.glitch.me/",
+    title: "Slack with ChatGPT",
+    github: "https://slack-clone-7a938.web.app/",
   },
 ];
 
@@ -48,7 +48,7 @@ const Portfolio = () => {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  GitHub
+                  Website
                 </a>
               </div>
             </article>
