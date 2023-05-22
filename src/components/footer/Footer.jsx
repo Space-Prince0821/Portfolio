54e7c62f230d378ce6a4be5bdc0b0952a2a16dbd
Prince <Prince.chowdhury.246@my.csun.edu>
2023-05-22 13:09:55 -0700

    Updated footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
             <ul className='permalinks'>
                 <li><a href='#'>Home</a></li>
                 <li><a href='#about'>About</a></li>
-                <li><a href='#experiece'>Experience</a></li>
+                <li><a href='#experiece'>Skills</a></li>
                 <li><a href='#services'>Services</a></li>
                 <li><a href='#portfolio'>Portfolio</a></li>
                 <li><a href='#testimonials'>Testimonials</a></li>
